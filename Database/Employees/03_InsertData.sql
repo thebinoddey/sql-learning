ff21bd005c9997814dbc3f66218045292a179864
Binod Dey <biniddey06@gmail.com>
2026-08-03 22:40:39 +0530

    sql: Insertion of data in tables

---
+++ Database/Employees/03_InsertData.sql
@@ -0,0 +1,19 @@
+USE sql_learning;
+
+INSERT INTO Departments VALUES
+(1,'IT'),
+(2,'HR'),
+(3,'Finance'),
+(4,'Marketing');
+
+INSERT INTO Employees VALUES
+(101,'Binod','Dey','M',20,60000,1),
+(102,'Rahul','Sharma','M',24,45000,2),
+(103,'Priya','Singh','F',22,75000,1),
+(104,'Aman','Verma','M',26,52000,3),
+(105,'Sneha','Gupta','F',25,68000,4),
+(106,'Rohit','Kumar','M',23,58000,1),
+(107,'Neha','Jain','F',27,82000,3),
+(108,'Ankit','Joshi','M',21,47000,2),
+(109,'Pooja','Mehta','F',24,62000,4),
+(110,'Arjun','Patel','M',29,91000,1);
