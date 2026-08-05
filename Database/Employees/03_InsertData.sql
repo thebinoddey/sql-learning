f1335986bcc3ac1027070ac7ff72542abbf15505
Binod Dey <biniddey06@gmail.com>
2026-08-05 22:48:42 +0530

    sql: Inserted more data

--- Database/Employees/03_InsertData.sql
+++ Database/Employees/03_InsertData.sql
@@ -17,3 +17,36 @@ INSERT INTO Employees VALUES
 (108,'Ankit','Joshi','M',21,47000,2),
 (109,'Pooja','Mehta','F',24,62000,4),
 (110,'Arjun','Patel','M',29,91000,1);
+
+INSERT INTO Projects VALUES 
+    (1, 'Project101', '2025-10-10', '2026-12-10', 20000, 1, 'Rahul Sharma'),
+    (2, 'Project103', '2025-10-15', '2026-04-10', 45000, 1, 'Kapil Kumar'),
+    (3, 'ProjectAB1', '2025-05-01', '2026-02-05', 25000, 1, 'Shailesh'),
+    (4, 'Project105', '2025-10-10', '2026-04-10', 40000, 1, 'Binod Dey'),
+    (5, 'Project101', '2025-05-01', '2025-12-01', 19000, 1, 'Priya Prakash'),
+    (6, 'Project103', '2025-03-15', '2025-04-15', 15000, 1, 'Jaanvi Verma'),
+    (7, 'Project101', '2026-01-10', '2026-12-10', 30500, 1, 'Kiran Sharma'),
+    (8, 'Project103', '2026-03-03', '2026-10-08', 50000, 1, 'Madhur Sirdhi'),
+    (9, 'Project101', '2025-11-10', '2026-04-04', 20200, 1, 'Rahul Singh'),
+    (10, 'Project103', '2025-10-15', '2026-04-10', 40000, 1, 'Dheeraj Kumar');
+
+
+    -- 2nd level insertion
+    INSERT INTO Departments VALUES 
+    (5,'Sales'),
+    (6,'AI'),
+    (7,'Data'),
+    (8,'Content');
+
+    INSERT INTO Employees VALUES
+    (111,'Gaurav','Kirad','M',26,60000,7),
+    (112,'Jaya','Sharma','F',24,55000,6),
+    (113,'Jyoti','Nathawat','F',28,70000,8),
+    (114,'Kanishk','Pal','M',30,78000,7),
+    (115,'Garvit','Jain','M',26,40000,6),
+    (116,'Kabir','Khan','M',31,70000,7),
+    (117,'Palash','Choudhary','M',28,25000, NULL),
+    (118,'Arohi','Jangid','F',26,42000,8);
+
+    INSERT INTO Departments VALUES 
+    (9, NULL);
